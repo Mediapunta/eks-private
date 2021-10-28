0f0c9e576f6163cacf85de7aba0239de4c30eea0
Helion <huyhjh@gmail.com>
2021-10-28 14:32:11 +0900

    module creation 모듈 생성
    각 서비스에 대한 모듈 생성
    Create a module for each service

--- eks-private-module/modules/securitygroup/securitygroup.tf
+++ eks-private-module/modules/securitygroup/securitygroup.tf
@@ -29,22 +29,3 @@ resource "aws_security_group_rule" "outbound_rule" {
 
   lifecycle { create_before_destroy = true }
 }
-
-# locals {
-#     csv_filename = "../../dev/security_group/sg_csv/${var.project}-${var.service}-sg.csv"
-#     csv = file("${local.csv_filename}")
-#     sg_rules = csvdecode(local.csv)
-# }
-
-# module "sg_rule" {
-#     source          = "./rule"
-#     for_each        = { for rule in local.sg_rules : rule.key=> rule }
-#     sg_id           = aws_security_group.sg_group.id
-#     rule_type       = each.value.rule_type
-#     from_port       = each.value.from_port
-#     to_port         = each.value.to_port
-#     protocol        = each.value.protocol
-#     src_or_dst_type = each.value.src_or_dst_type
-#     destination     = each.value.src_or_dst
-#     description     = each.value.desc
-# }
